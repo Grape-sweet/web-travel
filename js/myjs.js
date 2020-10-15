96c041e92c65d9e513361fe01273cb1b449717c9
namefhf <2797822107@qq.com>
2020-10-15 17:19:35 +0800

    first commit

---
+++ js/myjs.js
@@ -0,0 +1,22 @@
+// footer固定在页面底部
+$(function(){
+    function footerPosition(){
+        $("footer").removeClass("fixed-bottom");
+        var contentHeight = document.body.scrollHeight,//网页正文全文高度
+            winHeight = window.innerHeight;//可视窗口高度，不包括浏览器顶部工具栏
+        if(!(contentHeight > winHeight)){
+            //当网页正文高度小于可视窗口高度时，为footer添加类fixed-bottom
+            $("footer").addClass("fixed-bottom");
+        } else {
+            $("footer").removeClass("fixed-bottom");
+        }
+    }
+    footerPosition();
+    $(window).resize(footerPosition);
+});
+
+// 导航栏点击效果
+$('li').click(function () {
+    $(this).addClass('tabactive');
+    $(this).siblings().removeClass('tabactive');
+});
